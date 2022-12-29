8a8f5e67e275ae33e421cd6da6a0becb94dad09e
Alex Inácio <mcp30194@gmail.com>
2022-12-29 11:12:51 -0300

    Enviando arquivos e exercicios da Seção 1-Node

---
+++ Node/Seção-1/app.js
@@ -0,0 +1,15 @@
+var somaFuncao = require("./calculadora")
+
+function mult (a, b){
+    return a * b;
+}
+
+function sub(a, b){
+    return a - b;
+}
+
+function div (a, b){
+    return a/b;
+}
+
+console.log(somaFuncao(40, 10));
